7d095d97b6226355d1489519b82ac3313a59cbe2
takumi640 <78232405+takumi640@users.noreply.github.com>
2022-07-16 02:28:36 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,181 @@
+'use strict'
+
+{
+  const next = document.getElementById('next');
+  const set = document.getElementById('set');
+  const back = document.getElementById('back');
+
+  next.addEventListener('mouseover', () =>{
+    set.style.transform = 'translateX(-100%)';
+  });
+
+  back.addEventListener('mouseout', () =>{
+    set.style.transform = 'translateX(0px)';
+  });
+
+}
+{
+  const next2 = document.getElementById('next2');
+  const set2 = document.getElementById('set2');
+  const back2 = document.getElementById('back2');
+
+  next2.addEventListener('mouseover', () =>{
+    set2.style.transform = 'translateX(-100%)';
+  });
+
+  back2.addEventListener('mouseout', () =>{
+    set2.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next3 = document.getElementById('next3');
+  const set3 = document.getElementById('set3');
+  const back3 = document.getElementById('back3');
+
+  next3.addEventListener('mouseover', () =>{
+    set3.style.transform = 'translateX(-100%)';
+  });
+
+  back3.addEventListener('mouseout', () =>{
+    set3.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next4 = document.getElementById('next4');
+  const set4 = document.getElementById('set4');
+  const back4 = document.getElementById('back4');
+
+  next4.addEventListener('mouseover', () =>{
+    set4.style.transform = 'translateX(-100%)';
+  });
+
+  back4.addEventListener('mouseout', () =>{
+    set4.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next5 = document.getElementById('next5');
+  const set5 = document.getElementById('set5');
+  const back5 = document.getElementById('back5');
+
+  next5.addEventListener('mouseover', () =>{
+    set5.style.transform = 'translateX(-100%)';
+  });
+
+  back5.addEventListener('mouseout', () =>{
+    set5.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next6 = document.getElementById('next6');
+  const set6 = document.getElementById('set6');
+  const back6 = document.getElementById('back6');
+
+  next6.addEventListener('mouseover', () =>{
+    set6.style.transform = 'translateX(-100%)';
+  });
+
+  back6.addEventListener('mouseout', () =>{
+    set6.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next7 = document.getElementById('next7');
+  const set7 = document.getElementById('set7');
+  const back7 = document.getElementById('back7');
+
+  next7.addEventListener('mouseover', () =>{
+    set7.style.transform = 'translateX(-100%)';
+  });
+
+  back7.addEventListener('mouseout', () =>{
+    set7.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next8 = document.getElementById('next8');
+  const set8 = document.getElementById('set8');
+  const back8 = document.getElementById('back8');
+
+  next8.addEventListener('mouseover', () =>{
+    set8.style.transform = 'translateX(-100%)';
+  });
+
+  back8.addEventListener('mouseout', () =>{
+    set8.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next9 = document.getElementById('next9');
+  const set9 = document.getElementById('set9');
+  const back9 = document.getElementById('back9');
+
+  next9.addEventListener('mouseover', () =>{
+    set9.style.transform = 'translateX(-100%)';
+  });
+
+  back9.addEventListener('mouseout', () =>{
+    set9.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next10 = document.getElementById('next10');
+  const set10 = document.getElementById('set10');
+  const back10 = document.getElementById('back10');
+
+  next10.addEventListener('mouseover', () =>{
+    set10.style.transform = 'translateX(-100%)';
+  });
+
+  back10.addEventListener('mouseout', () =>{
+    set10.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next11 = document.getElementById('next11');
+  const set11 = document.getElementById('set11');
+  const back11 = document.getElementById('back11');
+
+  next11.addEventListener('mouseover', () =>{
+    set11.style.transform = 'translateX(-100%)';
+  });
+
+  back11.addEventListener('mouseout', () =>{
+    set11.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next12 = document.getElementById('next12');
+  const set12 = document.getElementById('set12');
+  const back12 = document.getElementById('back12');
+
+  next12.addEventListener('mouseover', () =>{
+    set12.style.transform = 'translateX(-100%)';
+  });
+
+  back12.addEventListener('mouseout', () =>{
+    set12.style.transform = 'translateX(0px)';
+  });
+
+}
+
---
+++ js/second.js
@@ -0,0 +1,46 @@
+'use strict'
+
+{
+  const next13 = document.getElementById('next13');
+  const set13 = document.getElementById('set13');
+  const back13 = document.getElementById('back13');
+
+  next13.addEventListener('mouseover', () =>{
+    set13.style.transform = 'translateX(-100%)';
+  });
+
+  back13.addEventListener('mouseout', () =>{
+    set13.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next14 = document.getElementById('next14');
+  const set14 = document.getElementById('set14');
+  const back14 = document.getElementById('back14');
+
+  next14.addEventListener('mouseover', () =>{
+    set14.style.transform = 'translateX(-100%)';
+  });
+
+  back14.addEventListener('mouseout', () =>{
+    set14.style.transform = 'translateX(0px)';
+  });
+
+}
+
+{
+  const next15 = document.getElementById('next15');
+  const set15 = document.getElementById('set15');
+  const back15 = document.getElementById('back15');
+
+  next15.addEventListener('mouseover', () =>{
+    set15.style.transform = 'translateX(-100%)';
+  });
+
+  back15.addEventListener('mouseout', () =>{
+    set15.style.transform = 'translateX(0px)';
+  });
+
+}
